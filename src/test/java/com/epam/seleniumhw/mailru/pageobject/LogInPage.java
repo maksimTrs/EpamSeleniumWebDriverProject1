4bbcf4be64cd24718d2eaf86c1b58dd1eee0e5a8
Maksim T <roronoazorroippo@gmail.com>
2023-01-30 18:20:45 +0400

    update log in case

--- src/test/java/com/epam/seleniumhw/mailru/pageobject/LogInPage.java
+++ src/test/java/com/epam/seleniumhw/mailru/pageobject/LogInPage.java
@@ -6,22 +6,31 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class LogInPage extends BasePage {
+public class MailRuLogInPage extends AbstractPage {
 
-    private static String logInIFrame = "//iframe[contains(@src, 'mail.ru/login')]";
 
     @FindBy(xpath = "//div[@data-testid='logged-out-one-click']/button[text()='Войти']")
     private WebElement logInButton;
+
+//    @FindBy(xpath = "//div/iframe[contains(@src, 'mail.ru/login')]")
+//    private WebElement logInPopUpIframe;
+
     @FindBy(css = "input[name='username']")
     private WebElement userNameLogInField;
+
     @FindBy(xpath = "//button[@data-test-id='next-button']")
     private WebElement userPasswordLogInButton;
+
     @FindBy(css = "input[name='password']")
     private WebElement userPasswordLogInField;
+
     @FindBy(xpath = "//button[@data-test-id='submit-button']")
     private WebElement logInSubmitButton;
 
-    public LogInPage(WebDriver driver) {
+    private static  String logInIFrame = "//iframe[contains(@src, 'mail.ru/login')]";
+
+
+    public MailRuLogInPage(WebDriver driver) {
         super(driver);
     }
 
@@ -31,6 +40,7 @@ public class LogInPage extends BasePage {
         webDriverWait.until(ExpectedConditions.elementToBeClickable(logInButton));
         logInButton.click();
 
+      //  driver.switchTo().frame(13);
         driver.switchTo().frame(driver.findElement(By.xpath(logInIFrame)));
 
         webDriverWait.until(ExpectedConditions.elementToBeClickable(userNameLogInField));
@@ -46,11 +56,5 @@ public class LogInPage extends BasePage {
         logInSubmitButton.click();
 
         driver.switchTo().defaultContent();
-    }
-
-    public boolean validateLogOut() {
-        webDriverWait.until(ExpectedConditions.elementToBeClickable(logInButton));
-
-        return (logInButton.isDisplayed() || logInButton.isEnabled());
     }
 }
